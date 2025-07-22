05131f4fd2c0f8e6a52434e86a21b9d5936b140e
tkdgh7063 <hoo412298@gmail.com>
2025-07-22 16:43:45 +0900

    feat: complete header UI with animation

---
+++ src/Components/Header.tsx
@@ -0,0 +1,238 @@
+import styled from "styled-components";
+import {
+  AnimatePresence,
+  motion,
+  useAnimation,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+  Variants,
+} from "motion/react";
+import { Link, useRouteMatch } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+
+const Nav = styled(motion.nav)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  font-size: 14px;
+  padding: 20px 60px;
+  color: white;
+`;
+
+const Col = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled(motion.svg)`
+  margin-right: 50px;
+  width: 95px;
+  height: 25px;
+  fill: ${(props) => props.theme.red};
+  path {
+    stroke-width: 6px;
+    stroke: white;
+  }
+`;
+
+const Items = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const Item = styled(motion.li)`
+  margin-right: 20px;
+  color: ${(props) => props.theme.white.darker};
+  transition: color 0.3s ease-in-out;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  &:hover {
+    color: ${(props) => props.theme.white.lighter};
+  }
+`;
+
+const Circle = styled(motion.span)`
+  height: 7px;
+  width: 7px;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.red};
+  position: absolute;
+  bottom: -14px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+`;
+
+const Search = styled.span`
+  color: white;
+  display: flex;
+  align-items: center;
+  position: relative;
+  svg {
+    height: 25px;
+  }
+`;
+
+const Input = styled(motion.input)`
+  transform-origin: right center;
+  position: absolute;
+  right: 0px;
+  padding: 5px 10px;
+  padding-left: 25px;
+  z-index: -1;
+  color: white;
+  font-size: 16px;
+  background-color: transparent;
+  border: 1px solid ${(props) => props.theme.white.lighter};
+  &:focus {
+    outline: none;
+  }
+`;
+
+const logoVariants: Variants = {
+  active: {
+    fillOpacity: [0, 0.6, 0, 0.8, 0, 0.4, 1, 0],
+    transition: {
+      duration: 0.9,
+      ease: "easeInOut",
+      repeat: Infinity,
+    },
+  },
+  normal: { fillOpacity: 1 },
+};
+
+const navVariants: Variants = {
+  top: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  scroll: { backgroundColor: "rgba(0, 0, 0, 1)" },
+};
+
+function Header() {
+  const [searchOpen, setSearchOpen] = useState(false);
+  const homeMatch = useRouteMatch("/");
+  const tvMatch = useRouteMatch("/tv");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const navAnimation = useAnimation();
+  const inputAnimation = useAnimation();
+  const svgAnimation = useAnimation();
+
+  const { scrollYProgress } = useScroll();
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    if (latest > 0.2) {
+      navAnimation.start("scroll");
+    } else {
+      navAnimation.start("top");
+    }
+  });
+
+  // const navBg = useTransform(
+  //   scrollYProgress,
+  //   [0, 1],
+  //   ["rgba(0, 0, 0, 1)", "rgba(0, 0, 0, 0)"]
+  // );
+
+  // const itemColor = useTransform(
+  //   scrollYProgress,
+  //   [0, 1],
+  //   ["rgba(255, 255, 255, 1)", "rgba(0, 0, 0, 1)"]
+  // );
+
+  // const inputBg = useTransform(
+  //   scrollYProgress,
+  //   [0, 1],
+  //   ["rgba(0, 0, 0, 1)", "rgba(255, 255, 255, 1)"]
+  // );
+
+  // const svgColor = useTransform(
+  //   scrollYProgress,
+  //   [0, 1],
+  //   searchOpen
+  //     ? ["rgba(0, 0, 0, 1)", "rgba(0, 0, 0, 0)"]
+  //     : ["rgba(255, 255, 255, 1)", "rgba(0, 0, 0, 1)"]
+  // );
+
+  const toggleSearch = () => setSearchOpen((prev) => !prev);
+  useEffect(() => {
+    if (searchOpen) {
+      inputAnimation.start({ scaleX: 1 });
+      svgAnimation.start({ x: -200, scale: 0.8 });
+    } else {
+      inputAnimation.start({ scaleX: 0 });
+      svgAnimation.start({ x: 0, scale: 1 });
+    }
+  }, [searchOpen, inputAnimation, svgAnimation]);
+
+  return (
+    <Nav variants={navVariants} initial="top" animate={navAnimation}>
+      <Col>
+        <Logo
+          whileHover="active"
+          variants={logoVariants}
+          initial="normal"
+          xmlns="http://www.w3.org/2000/svg"
+          width="1024"
+          height="276.742"
+          viewBox="0 0 1024 276.742">
+          <motion.path d="M140.803 258.904c-15.404 2.705-31.079 3.516-47.294 5.676l-49.458-144.856v151.073c-15.404 1.621-29.457 3.783-44.051 5.945v-276.742h41.08l56.212 157.021v-157.021h43.511v258.904zm85.131-157.558c16.757 0 42.431-.811 57.835-.811v43.24c-19.189 0-41.619 0-57.835.811v64.322c25.405-1.621 50.809-3.785 76.482-4.596v41.617l-119.724 9.461v-255.39h119.724v43.241h-76.482v58.105zm237.284-58.104h-44.862v198.908c-14.594 0-29.188 0-43.239.539v-199.447h-44.862v-43.242h132.965l-.002 43.242zm70.266 55.132h59.187v43.24h-59.187v98.104h-42.433v-239.718h120.808v43.241h-78.375v55.133zm148.641 103.507c24.594.539 49.456 2.434 73.51 3.783v42.701c-38.646-2.434-77.293-4.863-116.75-5.676v-242.689h43.24v201.881zm109.994 49.457c13.783.812 28.377 1.623 42.43 3.242v-254.58h-42.43v251.338zm231.881-251.338l-54.863 131.615 54.863 145.127c-16.217-2.162-32.432-5.135-48.648-7.838l-31.078-79.994-31.617 73.51c-15.678-2.705-30.812-3.516-46.484-5.678l55.672-126.75-50.269-129.992h46.482l28.377 72.699 30.27-72.699h47.295z" />
+        </Logo>
+        <Items>
+          <Item>
+            <Link to="/">
+              Home {homeMatch?.isExact && <Circle layoutId="dot" />}
+            </Link>
+          </Item>
+          <Item>
+            <Link to="/tv">
+              TV Shows {tvMatch && <Circle layoutId="dot" />}
+            </Link>
+          </Item>
+          <Item></Item>
+        </Items>
+      </Col>
+
+      <Col>
+        <Search>
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            fill="currentColor"
+            onClick={toggleSearch}
+            animate={svgAnimation}
+            transition={{ ease: "linear", duration: 0.2 }}>
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
+            />
+          </motion.svg>
+
+          <AnimatePresence>
+            {searchOpen && (
+              <Input
+                ref={inputRef}
+                onBlur={() => {
+                  setSearchOpen(false);
+                  if (inputRef.current) inputRef.current.value = "";
+                }}
+                initial={{ scaleX: 0 }}
+                animate={inputAnimation}
+                exit={{ scaleX: 0 }}
+                onAnimationComplete={() => inputRef.current?.focus()}
+                transition={{ ease: "linear", duration: 0.2 }}
+                placeholder="Search for movie or tv show..."
+              />
+            )}
+          </AnimatePresence>
+        </Search>
+      </Col>
+    </Nav>
+  );
+}
+
+export default Header;
