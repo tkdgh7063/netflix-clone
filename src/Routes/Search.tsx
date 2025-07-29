bb549a97892b2a35fa33af867b37aa494c50e7b5
tkdgh7063 <hoo412298@gmail.com>
2025-07-29 17:27:28 +0900

    Implement basic search results screen

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,9 +1,131 @@
+import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
+import { styled } from "styled-components";
+import {
+  getMultiSearch,
+  MovieResult,
+  MultiResult,
+  PersonResult,
+  TVResult,
+} from "../api";
+import { toTitleCase } from "../utils";
+
+const Wrapper = styled.div`
+  padding: 65px 205px;
+`;
+
+const Loader = styled.div``;
+
+const SearchResultsContainer = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SearchResultsTitle = styled.h2`
+  margin-bottom: 20px;
+`;
+
+const SearchCategoryTitle = styled.h3``;
+
+const MovieSearchContainer = styled.div`
+  min-height: 50px;
+  background-color: red;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TVSearchContainer = styled.div`
+  min-height: 50px;
+  background-color: orange;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const PersonSearchContainer = styled.div`
+  min-height: 50px;
+  background-color: blue;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Result = styled.div`
+  height: 20px;
+  margin-bottom: 10px;
+`;
 
 function Search() {
   const location = useLocation();
-  const keyword = new URLSearchParams(location.search).get("keyword");
-  return null;
+  const query = new URLSearchParams(location.search).get("keyword") as string;
+
+  const { isLoading, data } = useQuery<MultiResult>(
+    ["multi", query],
+    () => getMultiSearch(query),
+    {
+      enabled: !!query,
+    }
+  );
+
+  if (!isLoading && !data) {
+    return <div>No Results</div>;
+  }
+
+  const MoviesResult = data?.results.filter(
+    (item): item is MovieResult => item.media_type === "movie"
+  );
+  const TVsResult = data?.results.filter(
+    (item): item is TVResult => item.media_type === "tv"
+  );
+  const PeopleResult = data?.results.filter(
+    (item): item is PersonResult => item.media_type === "person"
+  );
+
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Searching...</Loader>
+      ) : (
+        <SearchResultsContainer>
+          <SearchResultsTitle>
+            {`Search results for '${toTitleCase(query)}'`}
+          </SearchResultsTitle>
+          <SearchCategoryTitle>
+            Movies({MoviesResult?.length})
+          </SearchCategoryTitle>
+          <MovieSearchContainer>
+            {MoviesResult &&
+              MoviesResult.length > 0 &&
+              MoviesResult?.map((item) => (
+                <Result key={item.id}>{item.title}</Result>
+              ))}
+          </MovieSearchContainer>
+          <SearchCategoryTitle>
+            TV Shows({TVsResult?.length})
+          </SearchCategoryTitle>
+          <TVSearchContainer>
+            {TVsResult &&
+              TVsResult?.length > 0 &&
+              TVsResult?.map((item) => (
+                <Result key={item.id}>{item.name}</Result>
+              ))}
+          </TVSearchContainer>
+          <SearchCategoryTitle>
+            Person({PeopleResult?.length})
+          </SearchCategoryTitle>
+          <PersonSearchContainer>
+            {PeopleResult &&
+              PeopleResult?.length > 0 &&
+              PeopleResult?.map((item) => (
+                <Result key={item.id}>{item.name}</Result>
+              ))}
+          </PersonSearchContainer>
+        </SearchResultsContainer>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Search;
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,13 @@
 const IMG_URL = "https://image.tmdb.org/t/p";
 
-export function makeImagePath(id: string, format?: string) {
+export function makeImagePath(id: string, format?: string): string {
   return `${IMG_URL}/${format ? format : "original"}/${id}`;
+}
+
+export function toTitleCase(str: string): string {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
